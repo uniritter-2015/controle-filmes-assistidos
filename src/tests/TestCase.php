da3ae17e86d0b331274ed9142f6a06afa32556e4
tiago <tiago@ubuntu-desktop>
2015-03-21 06:35:32 -0300

    Add - configurando testes.

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -15,5 +15,36 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return $app;
 	}
+	
+	/**
+	 * Default preparation for each test
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+	
+		$this->prepareForTests();
+	}
+	
+	/**
+	 * Migrates the database and set the mailer to 'pretend'.
+	 * This will cause the tests to run quickly.
+	 */
+	private function prepareForTests()
+	{
+		Artisan::call('migrate');
+		Artisan::call('db:seed');
+		Mail::pretend(true);
+	}
+	
+	public function __call($method, $args)
+	{
+		if (in_array($method, ['get', 'post', 'put', 'patch', 'delete']))
+		{
+			return $this->call($method, $args[0]);
+		}
+	
+		throw new BadMethodCallException;
+	}
 
 }
