6dc1f4374c5852dd81f1250bbba25a2e2780405c
tiago <tiago@ubuntu-desktop>
2015-03-21 18:31:44 -0300

    NEW - lista de generos de filmes.

---
+++ arquivos/generos.sql
@@ -0,0 +1,31 @@
+Ação
+Animação
+Aventura
+Chanchada
+Cinema catástrofe
+Comédia
+Comédia romântica
+Comédia dramática
+Comédia de ação
+Cult
+Dança
+Documentários (não é um gênero, é uma categoria)
+Drama
+Espionagem
+Erótico
+Fantasia
+Faroeste (ou western)
+Ficção científica
+Franchise/Séries
+Guerra
+Machinima
+Masala
+Musical
+Filme noir
+Policial
+Pornochanchada
+Pornográfico
+Romance
+Suspense
+Terror (ou horror)
+Trash
