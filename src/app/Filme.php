d6562acc82fd5209e9a8c78af896b1d3f181fe8d
Tiago Farias <tiago.farias.poa@gmail.com>
2015-03-21 21:20:22 -0300

    Add - lista de notas.

--- src/app/Filme.php
+++ src/app/Filme.php
@@ -5,7 +5,22 @@ use Illuminate\Database\Eloquent\Model;
 class Filme extends Model {
 
     protected $table = 'filmes';
-
+    
+    /**
+     * @return Array
+     */
+    public static function getNotas()
+    {
+    	$notas = [];
+    	for($i = 0; $i <= 10; $i++){
+    		
+    		$notas[$i] = $i;
+    	}
+    	
+    	return $notas;
+    }
+    
+    
     public function genero()
     {
         return $this->belongsTo('App\Genero');
