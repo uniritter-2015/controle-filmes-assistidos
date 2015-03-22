7040ee3be3b7d02cee31e298b6242c8a0e38088d
Tiago Farias <tiago.farias.poa@gmail.com>
2015-03-22 12:09:37 -0300

    Fix - parâmentro em link.

--- src/resources/views/filmes/index.blade.php
+++ src/resources/views/filmes/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 	<ul>
-		<li><h3>{!! Html::link(URL::to('form-cadastrar-filme'), 'Cadastrar') !!}</h3></li>
+		<li><h3>{!! Html::link(URL::to('form-cadastro'), 'Cadastrar') !!}</h3></li>
 	</ul>
 
     <h3>Filmes</h3>
@@ -11,7 +11,7 @@
     @foreach($filmes as $filme)
         <article>
             <h2>
-            	{!! Html::link( URL::to('form-cadastrar-filme'), 'Cadastrar') !!}
+            	{!! Html::link( URL::to('form-detalhes', [$filme->id]), $filme->nome) !!}
             </h2>
 
         </article>
