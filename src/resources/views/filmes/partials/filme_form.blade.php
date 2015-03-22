8aba3051e9f082dd29ccaa6ede46d24b2b17cffc
Tiago Farias <tiago.farias.poa@gmail.com>
2015-03-22 12:10:19 -0300

    Add - "required" no formulário de cadasro de filme.

--- src/resources/views/filmes/partials/filme_form.blade.php
+++ src/resources/views/filmes/partials/filme_form.blade.php
@@ -1,21 +1,26 @@
 	<div class="form-group">
         {!! Form::label('nome', 'Nome do Filme:') !!}
-        {!! Form::text('nome', null, ['class' => 'form-control']) !!}
+        {!! Form::text('nome', null, ['class' => 'form-control', 'autofocus', 'required']) !!}
     </div>
 
     <div class="form-group">
+        {!! Form::label('ano', 'Ano:') !!}
+        {!! Form::input('number', 'ano', null, ['class' => 'form-control', 'required']) !!}
+    </div>
+    
+    <div class="form-group">
         {!! Form::label('nota', 'Nota:') !!}
-        {!! Form::select('nota', $lista['notas'], '', ['class' => 'form-control']) !!}
+        {!! Form::select('nota', $lista['notas'], '', ['class' => 'form-control', 'required']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('genero_id', 'Genero:') !!}
-        {!! Form::select('genero_id', $lista['generos'], '', ['class' => 'form-control']) !!}
+        {!! Form::select('genero_id', $lista['generos'], '', ['class' => 'form-control', 'required']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('pais_id', 'País:') !!}
-        {!! Form::select('pais_id', $lista['paises'], '', ['class' => 'form-control']) !!}
+        {!! Form::select('pais_id', $lista['paises'], '', ['class' => 'form-control', 'required']) !!}
     </div>    
 
     <div class="form-group">
