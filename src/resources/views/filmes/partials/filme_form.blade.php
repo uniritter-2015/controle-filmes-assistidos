f37898f012548f4631a79f798dc27b1aaad37d25
Tiago Farias <tiago.farias.poa@gmail.com>
2015-03-23 04:46:45 -0300

    Fix - dados do filmes escolhido no formulário.

--- src/resources/views/filmes/partials/filme_form.blade.php
+++ src/resources/views/filmes/partials/filme_form.blade.php
@@ -10,15 +10,15 @@
     
     <div class="form-group">
         {!! Form::label('nota', 'Nota:') !!}
-        {!! Form::select('nota', $lista['notas'], '', ['class' => 'form-control text-capitalize', 'required']) !!}
+        {!! Form::selectRange('nota', 0, 10) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('genero_id', 'Genero:') !!}
-        {!! Form::select('genero_id', $lista['generos'], '', ['class' => 'form-control text-capitalize', 'required']) !!}
+        {!! Form::select('genero_id', $lista['generos'], isset($filme) ? $filme->genero->id : 0, ['class' => 'form-control text-capitalize', 'required']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('pais_id', 'País:') !!}
-        {!! Form::select('pais_id', $lista['paises'], '', ['class' => 'form-control text-capitalize', 'required']) !!}
+        {!! Form::select('pais_id', $lista['paises'], isset($filme) ? $filme->pais->id : 0, ['class' => 'form-control text-capitalize', 'required']) !!}
     </div>
