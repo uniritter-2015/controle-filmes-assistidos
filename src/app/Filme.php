8a9571c84d18eb88f0948b5d6271ebdfdbd4ec21
tiago farias <tiago.farias.poa@gmail.com>
2015-03-23 15:35:02 -0300

    Fix- upload de imagem do filme.

--- src/app/Filme.php
+++ src/app/Filme.php
@@ -1,10 +1,44 @@
 <?php namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class Filme extends Model {
 
+	private $file;
+	
     protected $table = 'filmes';
+
+    
+    public static function boot()
+    {
+    	parent::boot();
+    	
+    	static::created(function(Filme $filme){
+    		
+    		$file = $filme->getUploadFile();
+    		$imagem = $filme->imagem;
+    		
+    		$file->move( 'capas/', $file->getClientOriginalName() );
+    		
+    	});
+    }
+    
+    
+    public function getUploadFile()
+    {
+    	return $this->file;
+    }
+    public function setUploadFile(UploadedFile $file)
+    {
+    	$this->file = $file;
+    	$this->imagem = $file->getClientOriginalName();
+    }
+    
+    public function setImagemAttribute($imagem)
+    {
+    	$this->attributes['imagem'] = '/capas/'.trim($imagem);
+    } 
     
     
     public function genero()
