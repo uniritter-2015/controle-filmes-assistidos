76b3b5c6eeed7f73852828d9306672dc6a6c5c7e
Tiago Farias <tiago.farias.poa@gmail.com>
2015-03-22 22:13:16 -0300

    Fix - formulário de cadastro de filme.

--- src/resources/views/filmes/cadastrar.blade.php
+++ src/resources/views/filmes/cadastrar.blade.php
@@ -1,11 +1,22 @@
 @extends('app')
 
-@section('content')
+@section('titulo-secao')
+	Novo Filme
+@stop
+
+@section('conteudo')
 
 	@include('filmes.partials.erros')
 	
-	{!! Form::open(['url' => 'salvar', 'files' => true]) !!}
-    	@include('filmes.partials.filme_form')
-    {!! Form::close() !!}
+	<div class="row">
+		<div class="col-md-6">
+		{!! Form::open(['url' => 'salvar', 'files' => true]) !!}
+	    	@include('filmes.partials.filme_form')
+	    	 <div class="form-group">
+	        	{!! Form::submit('Adicionar Filme', ['class' => 'btn btn-sm btn-primary']) !!}
+	    	</div>
+	    {!! Form::close() !!}
+	    </div>
+	</div>
     
 @stop
