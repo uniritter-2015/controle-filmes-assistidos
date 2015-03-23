4a9e436372e51c8a730acec105cf4ecc4fa10fd0
Tiago Farias <tiago.farias.poa@gmail.com>
2015-03-22 22:12:07 -0300

    Fix - formatando elementos.

--- src/resources/views/filmes/partials/filme_form.blade.php
+++ src/resources/views/filmes/partials/filme_form.blade.php
@@ -10,19 +10,15 @@
     
     <div class="form-group">
         {!! Form::label('nota', 'Nota:') !!}
-        {!! Form::select('nota', $lista['notas'], '', ['class' => 'form-control', 'required']) !!}
+        {!! Form::select('nota', $lista['notas'], '', ['class' => 'form-control text-capitalize', 'required']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('genero_id', 'Genero:') !!}
-        {!! Form::select('genero_id', $lista['generos'], '', ['class' => 'form-control', 'required']) !!}
+        {!! Form::select('genero_id', $lista['generos'], '', ['class' => 'form-control text-capitalize', 'required']) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('pais_id', 'País:') !!}
-        {!! Form::select('pais_id', $lista['paises'], '', ['class' => 'form-control', 'required']) !!}
-    </div>    
-
-    <div class="form-group">
-        {!! Form::submit('Adicionar Filme', ['class' => 'btn btn-primary form-control']) !!}
+        {!! Form::select('pais_id', $lista['paises'], '', ['class' => 'form-control text-capitalize', 'required']) !!}
     </div>
