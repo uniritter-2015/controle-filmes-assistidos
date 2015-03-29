5e2a28a7d67f81435bd3b24a92d23d6ff694eb62
Tiago Farias <tiago.farias.poa@gmail.com>
2015-03-29 07:50:55 -0300

    Add - fomrulário de comentário no cadastro/edição do filme.

--- src/resources/views/filmes/cadastrar.blade.php
+++ src/resources/views/filmes/cadastrar.blade.php
@@ -8,15 +8,19 @@
 
 	@include('filmes.partials.erros')
 	
-	<div class="row">
-		<div class="col-md-6">
-		{!! Form::open(['url' => 'salvar', 'files' => true]) !!}
-	    	@include('filmes.partials.filme_form')
-	    	 <div class="form-group">
-	        	{!! Form::submit('Adicionar Filme', ['class' => 'btn btn-sm btn-primary']) !!}
-	    	</div>
-	    {!! Form::close() !!}
-	    </div>
-	</div>
+	<div class="well well-lg">
+		<div class="row">
+			<div class="col-md-6">
+			{!! Form::open(['url' => 'salvar', 'files' => true]) !!}
+		    	@include('filmes.partials.filme_form')
+		    	<hr>
+		    	@include('filmes.partials.comentario_form')
+		    	 <div class="form-group">
+		        	{!! Form::submit('Adicionar Filme', ['class' => 'btn btn-sm btn-primary']) !!}
+		    	</div>
+		    {!! Form::close() !!}
+		    </div>
+		</div>
+    </div>
     
 @stop
--- src/resources/views/filmes/editar.blade.php
+++ src/resources/views/filmes/editar.blade.php
@@ -8,18 +8,22 @@
 
 	@include('filmes.partials.erros')
 	
-	<div class="row">
-		<div class="col-md-6">
-		{!! Form::model($filme, ['url' => 'salvar/'.$filme->id, 'name' => 'form-editar-filme', 'files' => true]) !!}		
-	    	@include('filmes.partials.filme_form')
-	    	<div class="row">
-	    		<div class="col-md-12">
-	    			{!! Form::submit('Salvar', ['class' => 'btn btn-sm btn-primary']) !!}
-	    			{!! Html::link(URL::to('excluir', [$filme->id]), 'Excluir', ['title' => 'Excluir filme', 'name' => "excluir-filme", 'class' => 'btn btn-sm btn-danger pull-right']) !!}
-	    		</div>
-	    	</div>
-	    {!! Form::close() !!}
-	    </div>
+	<div class="well well-lg">
+		<div class="row">
+			<div class="col-md-6">
+			{!! Form::model($filme, ['url' => 'salvar/'.$filme->id, 'name' => 'form-editar-filme', 'files' => true]) !!}		
+		    	@include('filmes.partials.filme_form')
+		    	<hr>
+		    	@include('filmes.partials.comentario_form')
+		    	<div class="row">
+		    		<div class="col-md-12">
+		    			{!! Form::submit('Salvar', ['class' => 'btn btn-sm btn-primary']) !!}
+		    			{!! Html::link(URL::to('excluir', [$filme->id]), 'Excluir', ['title' => 'Excluir filme', 'name' => "excluir-filme", 'class' => 'btn btn-sm btn-danger pull-right']) !!}
+		    		</div>
+		    	</div>
+		    {!! Form::close() !!}
+		    </div>
+		</div>
 	</div>
     
 @stop
