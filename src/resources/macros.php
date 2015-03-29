a9a5d9c40db8e5dd5d0a7f1ce49fe99bd8632e87
Tiago Farias <tiago.farias.poa@gmail.com>
2015-03-29 20:39:21 -0300

    Fix - excluindo funções de macros.

--- src/resources/macros.php
+++ src/resources/macros.php
@@ -1,46 +1,14 @@
 <?php
 
-Form::macro('selectNotas', function($name, $default)
-{
-	$html  = '<select required="required" class="form-control" name="' .$name. '">';
+Form::macro('listarGenerosDoFilme', function(Array $generos){
 	
-	$html .= sprintf('<option %s value="-1">Selecione</option>', ($default == -1) ? 'selected = "selected"' : '');
-	for ($key = 0; $key <= 5; $key++) {
-		
-		$html .= sprintf('<option %s value="%s">%s</option>', ($key == $default) ? 'selected = "selected"' : '', $key, $key);
-	}
-	
-	$html .= '</select>';
-	
-	return $html;
-});
-
-Form::macro('selectGeneros', function($name, Array $generos, $default)
-{
-	$html  = '<select required="required" class="form-control text-capitalize" name="' .$name. '">';
-	
-	$html .= sprintf('<option %s value="0">Selecione</option>', ($default == 0) ? 'selected = "selected"' : '');
+	$lista = [];
 	foreach ($generos as $key => $genero) {
 		
-		$html .= sprintf('<option %s value="%s">%s</option>', ($key == $default) ? 'selected = "selected"' : '', $key, $genero);
+		$lista[$key] = $genero->nome;
 	}
 	
-	$html .= '</select>';
-
-	return $html;
-});
-
-Form::macro('selectPaises', function($name, Array $paises, $default)
-{
-	$html  = '<select required="required" class="form-control text-capitalize" name="' .$name. '">';
+	$valor = implode(', ', $lista);
 	
-	$html .= sprintf('<option %s value="0">Selecione</option>', ($default == 0) ? 'selected = "selected"' : '');
-	foreach ($paises as $key => $pais) {
-		
-		$html .= sprintf('<option %s value="%s">%s</option>', ($key == $default) ? 'selected = "selected"' : '', $key, $pais);
-	}
-	
-	$html .= '</select>';
-
-	return $html;
+	return $valor;
 });
