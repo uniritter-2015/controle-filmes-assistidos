53103d27565c10c33eba9e5b6cf68612507809e6
Tiago Farias <tiago.farias.poa@gmail.com>
2015-03-29 10:03:08 -0300

    Fix - substituindos por macros.

--- src/resources/macros.php
+++ src/resources/macros.php
@@ -1,16 +1,52 @@
 <?php
 
-Form::macro('selectNotas', function($name)
+Form::macro('selectNotas', function($name, $default)
 {
-	$html  = '<select class="form-control" name="$name">';
-	$html .= '<option selected="selected" value="-1">Selecione</option>';
-	$html .= '<option value="0">0</option>';
-	$html .= '<option value="1">1</option>';
-	$html .= '<option value="2">2</option>';
-	$html .= '<option value="3">3</option>';
-	$html .= '<option value="4">4</option>';
-	$html .= '<option value="5">5</option>';
+	$html  = '<select required="required" class="form-control" name="' .$name. '">';
+	
+	for ($key = -1; $key <= 5; $key++) {
+		
+		$selecionado = ($key == $default) ? 'selected = "selected"' : '';
+		$texto = ($key == -1) ? 'Selecione' : $key;
+		
+		$html .= sprintf('<option %s value="%s">%s</option>', $selecionado, $key, $texto);
+	}
+	
 	$html .= '</select>';
 	
+	return $html;
+});
+
+Form::macro('selectGeneros', function($name, Array $generos, $default)
+{
+	$html  = '<select required="required" class="form-control text-capitalize" name="' .$name. '">';
+		
+	foreach ($generos as $key => $genero) {
+		
+		$selecionado = ($key == $default) ? 'selected = "selected"' : '';
+		$texto = ($key == 0) ? 'Selecione' : $genero;
+		
+		$html .= sprintf('<option %s value="%s">%s</option>', $selecionado, $key, $texto);
+	}
+	
+	$html .= '</select>';
+
+	return $html;
+});
+
+Form::macro('selectPaises', function($name, Array $paises, $default)
+{
+	$html  = '<select class="form-control text-capitalize" name="' .$name. '">';
+
+	foreach ($paises as $key => $pais) {
+
+		$selecionado = ($key == $default) ? 'selected = "selected"' : '';
+		$texto = ($key == 0) ? 'Selecione' : $pais;
+
+		$html .= sprintf('<option %s value="%s">%s</option>', $selecionado, $key, $texto);
+	}
+
+	$html .= '</select>';
+
 	return $html;
 });
--- src/resources/views/filmes/partials/filme_form.blade.php
+++ src/resources/views/filmes/partials/filme_form.blade.php
@@ -13,21 +13,21 @@
     <div class="form-group">
         {!! Form::label('nota', 'Nota:') !!}
         <span class="req">*</span>
-        {!! Form::selectNotas('nota') !!}
+        {!! Form::selectNotas('nota', isset($filme) ? $filme->nota : -1) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('capa-filme', 'Imagem de Capa:') !!}
-        <input id="" type="file" name="capa-filme">
+        {!! Form::file('capa-filme', array('class' => '-form-control')) !!}
     </div>
     
     <div class="form-group">
         {!! Form::label('genero_id', 'Gênero:') !!}
         <span class="req">*</span>
-        {!! Form::select('genero_id', $lista['generos'], isset($filme) ? $filme->genero->id : 0, ['class' => 'form-control text-capitalize', 'required']) !!}
+        {!! Form::selectGeneros('genero_id', $lista['generos'], isset($filme) ? $filme->genero->id : 0) !!}
     </div>
 
     <div class="form-group">
         {!! Form::label('pais_id', 'País:') !!}
-        {!! Form::select('pais_id', $lista['paises'], isset($filme) ? $filme->pais->id : 0, ['class' => 'form-control text-capitalize', 'required']) !!}
+        {!! Form::selectPaises('pais_id', $lista['paises'], isset($filme) ? $filme->pais->id : 0) !!}
     </div>
