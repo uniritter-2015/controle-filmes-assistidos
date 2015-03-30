250592cafb35bb6b3f86de0b585172df616870b2
guitutilo <guitutilo_totty@hotmail.com>
2015-03-30 19:30:20 -0300

    database arrumada

--- src/database/migrations/2015_03_29_103002_create_filme_genero_table.php
+++ src/database/migrations/2015_03_29_103002_create_filme_genero_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFilmeGenerosTable extends Migration {
+class CreateFilmeGeneroTable extends Migration {
 
 	/**
 	 * Run the migrations.
