308f611c0742af9a5a8c6077aba9e4434d7fd932
Tiago Farias <tiago.farias.poa@gmail.com>
2015-04-04 23:23:27 -0300

    Fix - renomeando campos de formulário.

--- src/resources/views/filmes/adicionar.blade.php
+++ src/resources/views/filmes/adicionar.blade.php
@@ -11,10 +11,10 @@
 	<div class="well well-lg">
 		<div class="row">
 			<div class="col-md-6">
-			{!! Form::open(['url' => 'salvar', 'files' => true]) !!}
+			{!! Form::open(['url' => 'adicionar', 'files' => true]) !!}
 		    	@include('filmes.partials.filme_form')
 		    	<hr>
-		    	@include('filmes.partials.comentario_form')
+		    	@include('filmes.visualizacao.visualizacao_form')
 		    	 <div class="form-group">
 		        	{!! Form::submit('Adicionar Filme', ['class' => 'btn btn-sm btn-primary']) !!}
 		    	</div>
