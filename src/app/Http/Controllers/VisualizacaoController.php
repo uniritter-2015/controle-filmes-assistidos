84d442f71a2db686ff3d5a1e3981644a8da9a8ce
Tiago Farias <tiago.farias.poa@gmail.com>
2015-04-04 23:11:37 -0300

    Add - CRUD.

--- src/app/Http/Controllers/VisualizacaoController.php
+++ src/app/Http/Controllers/VisualizacaoController.php
@@ -2,13 +2,65 @@
 
 use App\Http\Controllers\Controller;
 use App\Filme;
+use App\Http\Requests\VisualizacaoRequest;
+use App\Visualizacao;
 
-class ComentariosController extends Controller {
+class VisualizacaoController extends Controller {
 	
-	public function getFormCadastro($filmeId)
+	public function getAdicionar($filmeId)
 	{
-		$form['filme'] = Filme::findOrFail($filmeId);
+		$filme = Filme::findOrFail($filmeId);
 		
-		return view('filmes.novo_comentario', $form);
+		return view('filmes.visualizacao.adicionar', compact('filme'));
+	}
+	
+	public function postAdicionar( VisualizacaoRequest $request, $filmeId )
+	{
+		$filme = Filme::FindOrFail( $filmeId );
+		
+		$visualizacao = new Visualizacao();
+		$visualizacao->comentario = $request->comentario;
+		$visualizacao->com_quem   = $request->com_quem;
+		$visualizacao->local 	  = $request->local;
+		$visualizacao->data 	  =	$request->data;
+		
+		\DB::transaction(function() use ($filme, $visualizacao){
+			 
+			$filme->save();
+			$visualizacao->filme()->associate( $filme )->save();
+		});
+			 
+		return redirect('/');
+		
+	}
+	
+	public function getEditar($visualizacaoId)
+	{
+		$visualizacao = Visualizacao::findOrFail( $visualizacaoId );
+	
+		return view('filmes.visualizacao.editar', compact('visualizacao') );
+	}
+	
+	public function postEditar(VisualizacaoRequest $request, $visualizacaoId)
+	{
+		$visualizacao = Visualizacao::findOrFail( $visualizacaoId );
+		$visualizacao->comentario = $request->comentario;
+		$visualizacao->com_quem   = $request->com_quem;
+		$visualizacao->local 	  = $request->local;
+		$visualizacao->data 	  =	$request->data;
+		
+		$visualizacao->save();
+		
+		return redirect('/');
+	}
+	
+	public function getExcluir($visualizacaoId)
+	{
+		\DB::transaction(function() use ($visualizacaoId){
+			 
+			Visualizacao::findOrFail( $visualizacaoId )->delete();
+		});
+		
+		return redirect('/');
 	}
 }
