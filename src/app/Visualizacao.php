a01375554c5d025ad2cfec96b2def31fe2e8924d
Tiago Farias <tiago.farias.poa@gmail.com>
2015-04-04 23:15:50 -0300

    Fix - excluindo ítens da visualizacao.

--- src/app/Visualizacao.php
+++ src/app/Visualizacao.php
@@ -6,11 +6,38 @@ use Illuminate\Database\Eloquent\Model;
 class Visualizacao extends Model {
 
     protected $table = 'visualizacoes';
+    
+	public static function boot()
+    {
+    	parent::boot();
+    	
+    	static::deleted(function(Visualizacao $visualizacao){
+    		
+    		if( $visualizacao->where('filme_id', $visualizacao->filme_id)->count() == 0 ){
+    			
+    			$visualizacao->filme->delete();
+    		}
+    	});
+    }
+    
+    public function setComentarioAttribute($value)
+    {
+    	$this->attributes['comentario'] = trim($value);
+    }
+    
+    public function setComQuemAttribute($value)
+    {
+    	$this->attributes['com_quem'] = trim($value);
+    }
+    
+    public function setLocalAttribute($value)
+    {
+    	$this->attributes['local'] = trim($value);
+    }
 
-    public function getDataVistoAttribute($date)
+    public function getDataAttribute($date)
     {
         return new \DateTime($date);
-
     }
 
     public function filme()
